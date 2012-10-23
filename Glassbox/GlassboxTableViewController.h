9a8a46d9f901c6593eedcd2deb336815af7ed476
Ken M. Haggerty <MCMDI@Kens-MacBook-Air.local>
2012-10-23 13:08:58 -0700

    Initial commit

---
+++ Glassbox/GlassboxTableViewController.h
@@ -0,0 +1,21 @@
+//
+//  GlassboxTableViewController.h
+//  Glassbox
+//
+//  Created by Ken M. Haggerty on 10/22/12.
+//  Copyright (c) 2012 Ken M. Haggerty. All rights reserved.
+//
+
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
+#import <UIKit/UIKit.h>
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
+
+@interface GlassboxTableViewController : UITableViewController
+
+@end
---
+++ Glassbox/GlassboxTableViewController.m
@@ -0,0 +1,149 @@
+//
+//  GlassboxTableViewController.m
+//  Glassbox
+//
+//  Created by Ken M. Haggerty on 10/22/12.
+//  Copyright (c) 2012 Ken M. Haggerty. All rights reserved.
+//
+
+#pragma mark - // NOTES (Private) //
+
+#pragma mark - // IMPORTS (Private) //
+
+#import "GlassboxTableViewController.h"
+
+#pragma mark - // DEFINITIONS (Private) //
+
+@interface GlassboxTableViewController ()
+@property (nonatomic, strong) NSMutableArray *listOfPlayers;
+@end
+
+@implementation GlassboxTableViewController
+
+#pragma mark - // SETTERS AND GETTERS //
+
+@synthesize listOfPlayers = _listOfPlayers;
+
+- (void)setListOfPlayers:(NSMutableArray *)listOfPlayers
+{
+    _listOfPlayers = listOfPlayers;
+}
+
+- (NSMutableArray *)listOfPlayers
+{
+    if (!_listOfPlayers)
+    {
+        _listOfPlayers = [[NSMutableArray alloc] init];
+        [_listOfPlayers addObject:[NSString stringWithFormat:@"Ken H."]];
+    }
+    return _listOfPlayers;
+}
+
+#pragma mark - // INITS AND LOADS //
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - // PRIVATE FUNCTIONS (Miscellaneous) //
+
+// TableView data source //
+
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+//#warning Potentially incomplete method implementation.
+//    // Return the number of sections.
+//    return 0;
+//}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.listOfPlayers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"GlassboxCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    cell.textLabel.text = [self.listOfPlayers objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = @"LOADED SUCCESSFULLY";
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+// TableView delegate //
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
